64a7ef5f057ea1e411d3d9fb2cbb81cf3096373e
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-12-13 21:48:11 +0200

    update

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -29,12 +29,3 @@ const SearchBar = ({ onChangeQuery }) => {
   );
 };
 export default SearchBar;
-/* <form>
-        <input
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-        <button type="submit">Search</button>
-      </form>*/
